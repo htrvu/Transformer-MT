0db55d6d9a0461e458a8e0d6d0790d9102c4ee00
huuthientran <huuthien013@gmail.com>
2023-06-10 18:55:41 +0700

    update: adapt with new args

--- base/metrics/bleu.py
+++ base/metrics/bleu.py
@@ -4,15 +4,13 @@ import torch.nn as nn
 
 from typing import List, Callable
 
+from tqdm import tqdm
+
 def bleu(
     valid_src: List[str],
     valid_trg: List[str],
-    model: nn.Module,
-    src_field: Field,
     trg_field: Field,
-    device: str,
     k: int,
-    max_len: int,
     callback: Callable
 ) -> float:
     """Calculate BLEU score
@@ -20,21 +18,15 @@ def bleu(
     Args:
         - valid_src (List[str]): Valid source sentences
         - valid_trg (List[str]): Valid target sentences
-        - model (nn.Module): Model to be evaluated
-        - src_field (Field): Tokenizer for source language
         - trg_field (Field): Tokenizer for target language
-        - device (str): Device to run model (e.g. cuda:0, cpu)
         - k (int): Number of beams
-        - max_len (int): Maximum length of sentence
-        - callback (Callable): Callback function to be called after each epoch
+        - callback (Callable): Callback function to be called after each epoch. 
 
     Returns: float: BLEU score
     """
     pred_sentences = []
-    for sentence in valid_src:
-        pred_trg = callback(
-            sentence, model, src_field, trg_field, device, k, max_len
-        )
+    for sentence in tqdm(valid_src):
+        pred_trg = callback(sentence, k)
         pred_sentences.append(pred_trg)
 
     pred_sentences = [trg_field.preprocess(sentence) for sentence in pred_sentences]
