00af7236bd4aff1f988d01b783999565eeaf18bf
huuthientran <huuthien013@gmail.com>
2023-06-24 15:07:28 +0700

    modify: re-format

--- base/trainer/trainer.py
+++ base/trainer/trainer.py
@@ -56,7 +56,6 @@ class Trainer:
 
         self.model.to(self.device)
 
-
     def step(self, batch: Batch) -> nn.Module:
         """
         A step of training
@@ -74,10 +73,12 @@ class Trainer:
         trg_inputs = trg[:, :-1]
         trg_labels = trg[:, 1:].contiguous().view(-1)
 
-        enc_padding_mask, dec_look_ahead_mask = gen_mask(src, 
-                                                            self.src_field.vocab.stoi[PAD], 
-                                                            trg_inputs,
-                                                            self.trg_field.vocab.stoi[PAD])
+        enc_padding_mask, dec_look_ahead_mask = gen_mask(
+            src,
+            self.src_field.vocab.stoi[PAD],
+            trg_inputs,
+            self.trg_field.vocab.stoi[PAD],
+        )
         preds, _, _ = self.model(src, trg_inputs, enc_padding_mask, dec_look_ahead_mask)
 
         self.optimizer.zero_grad()
@@ -86,7 +87,6 @@ class Trainer:
         self.optimizer.step()
 
         return loss
-
 
     @torch.no_grad()
     def validate(self, val_iter: Iterator) -> float:
@@ -108,12 +108,16 @@ class Trainer:
             trg_inputs = trg[:, :-1]
             trg_labels = trg[:, 1:].contiguous().view(-1)
 
-            enc_padding_mask, dec_look_ahead_mask = gen_mask(src, 
-                                                             self.src_field.vocab.stoi[PAD], 
-                                                             trg_inputs,
-                                                             self.trg_field.vocab.stoi[PAD])
+            enc_padding_mask, dec_look_ahead_mask = gen_mask(
+                src,
+                self.src_field.vocab.stoi[PAD],
+                trg_inputs,
+                self.trg_field.vocab.stoi[PAD],
+            )
 
-            preds, _, _ = self.model(src, trg_inputs, enc_padding_mask, dec_look_ahead_mask)
+            preds, _, _ = self.model(
+                src, trg_inputs, enc_padding_mask, dec_look_ahead_mask
+            )
 
             loss = self.criterion(preds.view(-1, preds.size(-1)), trg_labels)
             total_loss.append(loss.item())
@@ -122,8 +126,14 @@ class Trainer:
 
         return avg_loss
 
-
-    def fit(self, train_iter: Iterator, valid_iter: Iterator, beam_size: int = 1, out_dir: str = './runs', log_interval: int = 100) -> None:
+    def fit(
+        self,
+        train_iter: Iterator,
+        valid_iter: Iterator,
+        beam_size: int = 1,
+        out_dir: str = "./runs",
+        log_interval: int = 100,
+    ) -> None:
         """
         Fit model
 
@@ -132,19 +142,19 @@ class Trainer:
             valid_iter (Iterator): Valid iterator
             k (int): Beam size
         """
-        print('Start training...')
-        print('The training results will be saved at', out_dir)
+        print("Start training...")
+        print("The training results will be saved at", out_dir)
 
         # Only calculate BLEU on first 500 sentences of validation set
-        val_src = [' '.join(x.src) for x in valid_iter.dataset.examples[:500]][1:]
-        val_trg = [' '.join(x.trg) for x in valid_iter.dataset.examples[:500]][1:]
+        val_src = [" ".join(x.src) for x in valid_iter.dataset.examples[:500]][1:]
+        val_trg = [" ".join(x.trg) for x in valid_iter.dataset.examples[:500]][1:]
         predictor = Predictor(self.model, self.src_field, self.trg_field, self.device)
 
         log = []
         train_losses = []
         valid_losses = []
         bleu_scores = []
-        min_valid_loss = float('inf')
+        min_valid_loss = float("inf")
         for epoch in range(self.num_epochs):
             # Train
             epoch_loss = 0
@@ -161,7 +171,7 @@ class Trainer:
                     avg_loss = iter_loss / log_interval
                     print(
                         f"Epoch: {epoch + 1:3}/{self.num_epochs} | Iteration: {i} | Loss: {avg_loss:.4f}",
-                        end='\r'
+                        end="\r",
                     )
                     iter_loss = 0
 
@@ -172,50 +182,56 @@ class Trainer:
 
             # BLEU score
             pred_sentences = []
+            
             for sentence in val_src:
                 pred_trg = predictor(sentence, self.max_len, beam_size)
                 pred_sentences.append(pred_trg)
-            pred_sentences = [self.trg_field.preprocess(sentence) for sentence in pred_sentences]
+
+            pred_sentences = [
+                self.trg_field.preprocess(sentence) for sentence in pred_sentences
+            ]
             trg_sentences = [[sent.split()] for sent in val_trg]
             bleu_score = self.metric(pred_sentences, trg_sentences)
-            
+
             print(
                 f"Epoch: {epoch + 1:3}/{self.num_epochs} | Time: {time.time() - s:.2f}s | Train Loss: {avg_loss:.4f} | Valid Loss: {valid_loss:.4f} | BLEU score: {bleu_score}"
             )
 
-            log.append({
-                'epoch': epoch + 1,
-                'train_loss': avg_loss,
-                'val_loss': valid_loss,
-                'bleu_score': bleu_score
-            })
+            log.append(
+                {
+                    "epoch": epoch + 1,
+                    "train_loss": avg_loss,
+                    "val_loss": valid_loss,
+                    "bleu_score": bleu_score,
+                }
+            )
             train_losses.append(avg_loss)
             valid_losses.append(valid_loss)
             bleu_scores.append(bleu_score)
-            
+
             # Write log
-            with open(os.path.join(out_dir, 'log.json'), 'w') as f:
+            with open(os.path.join(out_dir, "log.json"), "w") as f:
                 json.dump(log, f, indent=4)
 
             # Save checkpoint
             if valid_loss < min_valid_loss:
                 min_valid_loss = valid_loss
-                torch.save(self.model.state_dict(), f'{out_dir}/best.pt')
-            torch.save(self.model.state_dict(), f'{out_dir}/last.pt')
+                torch.save(self.model.state_dict(), f"{out_dir}/best.pt")
+            torch.save(self.model.state_dict(), f"{out_dir}/last.pt")
 
             # Plot: Loss
             plt.figure()
-            plt.plot(train_losses, label='Train Loss')
-            plt.plot(valid_losses, label='Val Loss')
+            plt.plot(train_losses, label="Train Loss")
+            plt.plot(valid_losses, label="Val Loss")
             plt.legend()
-            plt.xlabel('Epoch')
-            plt.title('Loss')
-            plt.savefig(os.path.join(out_dir, 'loss_plot.png'))
+            plt.xlabel("Epoch")
+            plt.title("Loss")
+            plt.savefig(os.path.join(out_dir, "loss_plot.png"))
 
             # Plot: BLEU score
             plt.figure()
-            plt.plot(bleu_scores, label='BLEU score')
+            plt.plot(bleu_scores, label="BLEU score")
             plt.legend()
-            plt.xlabel('Epoch')
-            plt.title(f'BLEU score with beam size = {beam_size}')
-            plt.savefig(os.path.join(out_dir, 'bleu_plot.png'))
+            plt.xlabel("Epoch")
+            plt.title(f"BLEU score with beam size = {beam_size}")
+            plt.savefig(os.path.join(out_dir, "bleu_plot.png"))
